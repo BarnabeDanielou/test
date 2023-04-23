9ed327fe0c8f53de33a7c3bf34cca89bda73e304
Barnabe <131029112+BarnabeDanielou@users.noreply.github.com>
2023-04-23 09:14:57 -0600

    Add files via upload

---
+++ assets/js/translations.js
@@ -0,0 +1,12 @@
+const translations = {
+    fr: {
+      rol: 'Etudiant L3 Managament des Affaires et du Commerce International',
+    },
+    en: {
+      rol: 'Student L3 Managament of International Business',
+    },
+    es: {
+      rol: 'Etudiante L3 Negocios Internacionales',
+    },
+  };
+  
